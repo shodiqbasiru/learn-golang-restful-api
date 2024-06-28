ac05a2962b59055b898eddeaf87738e18e04e9ac
shodiqbasiru <shodiqbasiru@gmail.com>
2024-06-28 17:35:43 +0700

    learn golang migrate

--- app/database.go
+++ app/database.go
@@ -7,7 +7,7 @@ import (
 )
 
 func NewDB() *sql.DB {
-	db, err := sql.Open("mysql", "root:ShodiqB20@tcp(localhost:3306)/db_golang_restful_api")
+	db, err := sql.Open("mysql", "root:ShodiqB20@tcp(localhost:3306)/db_golang_migration")
 	helper.PanicIfError(err)
 
 	db.SetMaxIdleConns(5)
@@ -15,4 +15,13 @@ func NewDB() *sql.DB {
 	db.SetConnMaxLifetime(60 * time.Minute)
 	db.SetConnMaxIdleTime(10 * time.Minute)
 	return db
+
+	// migrate -database "mysql://root:ShodiqB20@tcp(localhost:3306)/db_golang_migration" -path db/migrations up
+	// migrate -database "mysql://root:ShodiqB20@tcp(localhost:3306)/db_golang_migration" -path db/migrations down
+
+	// migrate -database "mysql://root:ShodiqB20@tcp(localhost:3306)/db_golang_migration" -path db/migrations up 2
+	// migrate -database "mysql://root:ShodiqB20@tcp(localhost:3306)/db_golang_migration" -path db/migrations down 1
+
+	// migrate -database "mysql://root:ShodiqB20@tcp(localhost:3306)/db_golang_migration" -path db/migrations version
+	// migrate -database "mysql://root:ShodiqB20@tcp(localhost:3306)/db_golang_migration" -path db/migrations force 20240628070505
 }
